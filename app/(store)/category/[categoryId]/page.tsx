8182616c58e1d30b396d51a2d8e220fb989d0610
Earl A. Villapaz <jasonburning16@gmail.com>
2023-07-27 21:00:56 +0800

    filter

---
+++ app/(store)/category/[categoryId]/page.tsx
@@ -0,0 +1,77 @@
+
+import getColors from "@/actions/getColors";
+import getProducts from "@/actions/getProducts";
+import getSizes from "@/actions/getSizes";
+import getIndividualCategory from "@/actions/getIndividualCategory";
+
+import Filters from "@/components/category/Filters";
+import Billboard from "@/components/database/Billboard";
+import Container from "@/components/container/Containers";
+import NoResults from "@/components/container/NoResults";
+import ProductCards from "@/components/container/ProductCards";
+
+export const revalidate = 0;
+
+interface CategoryPageProps {
+    params: {
+        categoryId: string;
+    },
+    searchParams: {
+        colorId: string;
+        sizeId: string;
+    }
+};
+
+const CategoryPage: React.FC<CategoryPageProps> = async ({
+    params,
+    searchParams
+}) => {
+    const sizes = await getSizes();
+    const colors = await getColors();
+    const category = await getIndividualCategory(params.categoryId);
+
+    const products = await getProducts({
+        categoryId: params.categoryId,
+        colorId: searchParams.colorId,
+        sizeId: searchParams.sizeId
+    });
+
+    return ( 
+        <div className="bg-white">
+            <Container>
+                <Billboard 
+                    data={category.billboard}
+                />
+                <div className="px-4 sm:px-6 lg:px-8 pb-24">
+                    <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
+                        <div className="hidden lg:block">
+                            <Filters 
+                                name="Sizes"
+                                valueKey="sizeId"
+                                data={sizes}
+                            />
+                            <Filters 
+                                name="Colors"
+                                valueKey="colorId"
+                                data={colors}
+                            />
+                        </div>
+                        <div className="mt-6 lg:mt-0 lg:col-span-4">
+                            {products.length === 0 && <NoResults />}
+                            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                                {products.map((product) => (
+                                    <ProductCards 
+                                        key={product.id}
+                                        data={product}
+                                    />
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Container>
+        </div>
+    );
+}
+ 
+export default CategoryPage;
---
+++ components/category/Filters.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
+import qs from "query-string";
+
+import { Color, Size } from "@/utils/types";
+
+import Button from "@/components/container/Buttons";
+import { cn } from "@/lib/utils";
+
+interface FiltersProps {
+    name: string;
+    valueKey: string;
+    data: (Size | Color)[];
+};
+
+const Filters: React.FC<FiltersProps> = ({
+    name,
+    valueKey,
+    data
+}) => {
+    const searchParams = useSearchParams();
+    const router = useRouter();
+
+    const onSelectedValue = searchParams.get(valueKey);
+
+    const onClick = (id: string) => {
+        const current = qs.parse(searchParams.toString());
+
+        const query = {
+            ...current,
+            [valueKey]: id,
+        }
+
+        if (current[valueKey] === id) {
+            query[valueKey] = null;
+        }
+
+        const url = qs.stringifyUrl({
+            url: window.location.href,
+            query
+        }, { skipNull: true });
+
+        router.push(url);
+    };
+
+    return ( 
+        <div className="mb-8">
+            <h3 className="text-lg font-semibold">
+                {name}
+            </h3>
+            <hr className="my-4" />
+            <div className="flex flex-wrap gap-2">
+                {data.map((filter) => (
+                    <div key={filter.id} className="flex items-center">
+                        <Button className={cn("rounded-md text-sm text-gray-800 p-2 bg-white border border-gray-300",
+                            onSelectedValue === filter.id && "bg-black text-white"
+                        )}
+                        onClick={() => onClick(filter.id)}
+                        >
+                            {filter.name}
+                        </Button>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+ 
+export default Filters;
