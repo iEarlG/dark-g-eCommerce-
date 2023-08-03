bfab765a089897cef42d9da06f49872f9c9882ae
Earl A. Villapaz <jasonburning16@gmail.com>
2023-08-03 20:56:15 +0800

    final but to be adjusted soon

--- components/cart/SummaryInfo.tsx
+++ components/cart/SummaryInfo.tsx
@@ -48,7 +48,7 @@ const SummaryInfo = () => {
                     <CurrencyTag value={totalPrice} />
                 </div>
             </div>
-            <Button className="w-full mt-6" onClick={onCheckout}>
+            <Button className="w-full mt-6" onClick={onCheckout} disabled={items.length === 0}>
                 Checkout
             </Button>
         </div>
